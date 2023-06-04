8f56a562413c78f0d9848aeec460644dd004fdb0
Rabeet Sati <rabeetsati@gmail.com>
2023-06-05 00:43:20 +0500

    commit

--- API/Routes/UserRoutes.js
+++ API/Routes/UserRoutes.js
@@ -6,12 +6,56 @@ const bcrypt = require('bcryptjs')
 
 
 
-route.post(('/signup'), async (req, res) => {
+// route.post('/signup', async (req, res) => {
 
-    const { email, userName, password } = req.body
-    let obj = { email, password, userName };
+//         const { email, userName, password } = req.body
+//         let obj = { email, password, userName };
+//         let reqArray = ['email', 'password', 'userName'];
+//         let errArr = [];
+
+//         reqArray.forEach((objects) => {
+//             if (!obj[objects]) {
+//                 errArr.push(objects)
+//             }
+//         })
+
+//         if (errArr > 0) {
+//             res.send(SendResponse(false, null, 'Some fields are missing')).status(403)
+//         }
+
+//         else {
+//             const hashpassword = await bcrypt.hash(obj.password, 10)
+//             obj.password = hashpassword;
+
+//             const ExistingUser = await UserModel.findOne({ email });
+
+//             if (ExistingUser) {
+//                 res.send(SendResponse(false, null, 'this email is already in use')).status(404)
+//             }
+//             else {
+//                 UserModel.create(obj)
+//                     .then((user) => {
+//                         res.send(SendResponse(true, user, 'User Created ')).status(200)
+//                     })
+//                     .catch((err) => {
+//                         res.send(SendResponse(false, null, 'Cridentials Error', err)).status(404)
+//                     })
+//             }
+
+//         }
+//     })
+
+route.post('/signup', async (req, res) => {
+    const { email, userName, password } = req.body;
     let reqArray = ['email', 'password', 'userName'];
     let errArr = [];
+
+    // Validate the request payload
+    if (!email || !userName || !password) {
+        res.status(400).send(SendResponse(false, null, 'Missing required fields'));
+        return;
+    }
+    console.log('Received request:', email, userName, password);
 
     reqArray.forEach((objects) => {
         if (!obj[objects]) {
@@ -41,11 +85,18 @@ route.post(('/signup'), async (req, res) => {
                     res.send(SendResponse(false, null, 'Cridentials Error', err)).status(404)
                 })
         }
-
     }
-})
+});
+//   By implementing these steps and examining the debug information, you should be able to identify and resolve the issue causing the "Illegal arguments" error.
 
-route.post(('/login'), async (req, res) => {
+
+
+
+
+
+
+
+route.post('/login', async (req, res) => {
     const { email, password } = req.body;
     let obj = { email, password };
 
--- API/server.js
+++ API/server.js
@@ -8,7 +8,8 @@ const app = express()
 app.use(express.json())
 
 app.use('/api/students', StuRoutes)
-app.use('/api/user',UserRoute)
+app.use('/api/user',UserRoute);
+
 
 
 //Sources Link
